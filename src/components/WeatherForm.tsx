c2c11a3abf01f7868e3f5edd157c6295fcd45880
budi <bpratomo@myrsm.nl>
2021-06-27 07:35:17 +0200

    freshen up the form

--- src/components/WeatherForm.tsx
+++ src/components/WeatherForm.tsx
@@ -1,4 +1,3 @@
-import { Button, TextField } from "@material-ui/core";
 import { ChangeEventHandler, FC, SyntheticEvent, useState } from "react";
 
 type WeatherFormProps = {
@@ -6,11 +5,12 @@ type WeatherFormProps = {
 };
 
 export const WeatherForm: FC<WeatherFormProps> = ({ updateWeather }) => {
-  const [formValue, setFormValue] = useState<string | null>("Which city?");
+  const [formValue, setFormValue] =
+    useState<string | undefined>("Where do you live?");
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setFormValue(e.currentTarget.value)
-  }
+    setFormValue(e.currentTarget.value);
+  };
 
   function handleSubmit(e: SyntheticEvent) {
     e.preventDefault();
@@ -19,18 +19,72 @@ export const WeatherForm: FC<WeatherFormProps> = ({ updateWeather }) => {
     }
   }
 
+  function clearValue() {
+    setFormValue("");
+  }
+
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent:"space-between",
+    height: "100px",
+
+
+  };
+
+  const baseStyle = {
+    display: "flex",
+    flexDirection: "column" as "column",
+    justifyContent:"space-between",
+    
+
+  }
+
+  const textStyle = {
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: "10px",
+    height:"40px",
+    paddingLeft:"5px",
+    fontSize:"x-large",
+    fontFamily:"Itim"
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#6A2C70",
+    color:"white",
+    borderRadius: "10px",
+    height:"40px",
+    border:"none",
+    fontSize:"x-large",
+    fontFamily:"Itim"
+  }
+
+  const headerStyle = {
+    fontSize:"xx-large",
+    marginBottom:"20px"
+  }
+
   return (
-    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-      <TextField
-        id="outlined-basic"
-        label="Outlined"
-        variant="outlined"
-        value={formValue}
-        onChange={handleChange}
-      />
-      <Button variant="contained" color="primary" onClick={handleSubmit}>
-        Update
-      </Button>
-    </form>
+    <div className="WeatherForm" style={baseStyle} >
+      <header style={headerStyle}>Welcome to Sunrise!</header>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+        style={formStyle}
+      >
+        <input
+          type="text"
+          value={formValue}
+          onChange={handleChange}
+          onFocus={clearValue}
+          style={textStyle}
+        />
+        <button onClick={handleSubmit} style={buttonStyle}>
+          Update
+        </button>
+      </form>
+    </div>
   );
 };
